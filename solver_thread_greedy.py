7a0273f9f59e2008fbbaaf9e99c2bcaae922eba9
MiyabiTane <miyabitane@yahoo.co.jp>
2020-06-29 21:07:44 +0900

    using thread

--- solver_thread_greedy.py
+++ solver_thread_greedy.py
@@ -5,7 +5,8 @@ import math
 import time
 import numpy as np
 
-from concurrent.futures import ThreadPoolExecutor 
+import random 
+import threading
 
 from common import print_tour, read_input
 
@@ -14,7 +15,7 @@ def distance(city1, city2):
     return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
 
 
-def sub_greedy(index, tour, unvisited_cities, dist, current_city):
+def sub_greedy(index, tour, unvisited_cities, dist, current_city, results):
     #6都市先に進んだ時の長さを求める。
     sub_tour = tour.copy()
     sub_unvisited_cities = unvisited_cities.copy()
@@ -23,13 +24,14 @@ def sub_greedy(index, tour, unvisited_cities, dist, current_city):
     cand_cities = sorted(sub_unvisited_cities,
                          key=lambda city: dist[current_city][city])
     if index+1 >= len(cand_cities):
-        return None, None, None
+        results.append([None, None, None])
+        return
     next_city = cand_cities[index]
     distance += dist[current_city][next_city]
     sub_unvisited_cities.remove(next_city)
     sub_tour.append(next_city)
     current_city = next_city
-    for _ in range(5):
+    for _ in range(5): #need tuning
         if not sub_unvisited_cities:
             break
         next_city = min(sub_unvisited_cities,
@@ -38,7 +40,7 @@ def sub_greedy(index, tour, unvisited_cities, dist, current_city):
         sub_unvisited_cities.remove(next_city)
         sub_tour.append(next_city)
         current_city = next_city
-    return sub_tour, sub_unvisited_cities, distance
+    results.append([sub_tour, sub_unvisited_cities, distance])
 
 
 def remove_cross(tour, dist, flag = True):
@@ -62,11 +64,11 @@ def remove_cross(tour, dist, flag = True):
         new_tour = remove_cross(copy_tour + [start_city], dist, flag = False)
         if len(new_tour) != N:
             new_tour = copy_tour
-        tour = [start_city] + new_tour
+        tour = new_tour
     return tour
 
 
-def solve_helper(cities, start_i):
+def solve_helper(cities, start_i, tours):
     print("start : ", start_i)
     N = len(cities)
 
@@ -76,22 +78,28 @@ def solve_helper(cities, start_i):
             dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
 
     current_city = start_i
-    unvisited_cities = set(range(1, N))
+    unvisited_cities = [i for i in range(N)]
+    unvisited_cities.remove(start_i)
     tour = [current_city]
 
     flag = 0
+    #Threadを使って15個の経路を同時探索し、最短のものを採用する。
     while unvisited_cities and flag == 0:
-        how_far = [0,1,2,3,4]
-        executor = ThreadPoolExecutor(max_workers=5)
-        can_tour = []
-        can_unvisited_cities = []
-        distances = []
-        #Threadを使って５つの経路を同時探索し、最短のものを採用する。
-        for far in how_far:
-            future = executor.submit(sub_greedy, far, tour, unvisited_cities, dist, current_city)
-            can_tour.append(future.result()[0])
-            can_unvisited_cities.append(future.result()[1])
-            distances.append(future.result()[2])
+        thread_list = []
+        results = []
+        for i in range(15): #need tuning
+            thread = threading.Thread(target=sub_greedy, args=(
+                [i, tour, unvisited_cities, dist, current_city, results]))
+            thread_list.append(thread)
+        for thread in thread_list:
+            thread.start()
+        for thread in thread_list:
+            thread.join()
+        #経路が一番短いものを採用
+        results = np.array(results)
+        distances = results[:,2]
+        can_tour = results[:,0]
+        can_unvisited_cities = results[:,1]
         if None in distances:
             while unvisited_cities:
                 next_city = min(unvisited_cities,
@@ -106,27 +114,39 @@ def solve_helper(cities, start_i):
             unvisited_cities = can_unvisited_cities[mi]
     tour = remove_cross(tour, dist)
     print("finish : ", start_i)
-    return tour
+    tours.append(tour)
 
 
 def solve(cities):
-    tours = []
     N = len(cities)
-    executor = ThreadPoolExecutor(max_workers=N)
-    start_index = [i for i in range(N)]
     #threadを用いて各スタート地点から始めた場合の経路を同時探索する
-    for start_i in start_index:
-        future = executor.submit(solve_helper, cities, start_i)
-        tours.append(future.result())
+    #N>128ならランダムに10個のスタート地点を選ぶ
+    start_list = [i for i in range(N)]
+    if N > 128:
+        #start_list = random.sample(start_list, 10)
+        start_list = random.sample(start_list, 2)
+    thread_list = []
+    tours = []
+    for start_i in start_list:
+        thread = threading.Thread(target=solve_helper, args=([cities, start_i, tours]))
+        thread_list.append(thread)
+    for thread in thread_list:
+        thread.start()
+    for thread in thread_list:
+        thread.join()
     #経路が一番短いものを採用
     ans_tour = None
     min_length = float('inf')
-    for j in range(N):
-        tour = tours[i]
-        path_l = sum(distance(cities[tour[i]], cities[tour[(i + 1) % N]]) for i in range(N))
-        if path_l < min_length:
-            min_length = path_l
-            ans_tour = tour
+    for j in range(len(start_list)):
+        tour = tours[j]
+        try:
+            path_l = sum(distance(cities[tour[i]], cities[tour[(i + 1) % N]]) for i in range(N))
+            if path_l < min_length:
+                min_length = path_l
+                ans_tour = tour
+        except:
+            pass
+    print("path_length : ", min_length)
     return ans_tour
 
 
